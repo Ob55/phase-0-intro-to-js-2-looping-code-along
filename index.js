f592fbf15003cc6cb0d0b63022276dd468ca177a
Ob55 <brian55mwangi@gmail.com>
2023-12-17 20:31:45 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+function writeCards(brian, help) {
+  return brian.map(brian => `Thank you, ${brian}, for the wonderful ${help} gift!`);
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
